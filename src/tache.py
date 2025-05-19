3a85e513bc686be777fc3179cf8d593174f866b4
Ludovic Courtin <ludovic.courtin@etu.univ-tours.fr>
2025-05-19 11:10:49 +0200

    Ajout de la classe Tache pour la gestion des tâches avec des attributs de base

---
+++ src/tache.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Tache:
+    def __init__(self, id: str, duree: int, prealables: List[str]):
+        self.id = id                          # ID de la tâche
+        self.duree = duree                    # Durée en semaines
+        self.prealables = prealables          # Liste des ID des tâches préalables
+        self.debut = None                     # Calculé par l’algorithme
+        self.fin = None                       # Calculé par l’algorithme
+
