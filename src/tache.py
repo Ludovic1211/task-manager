ab8076ec8baa0d2eba2658cf0d9b9f75b0685867
Ludovic Courtin <ludovic.courtin@etu.univ-tours.fr>
2025-05-19 13:29:11 +0200

    Ajout de la méthode set_debut à la classe Tache et création de tests unitaires pour la classe Tache

--- src/tache.py
+++ src/tache.py
@@ -8,3 +8,8 @@ class Tache:
         self.debut = None                     # Calculé par l’algorithme
         self.fin = None                       # Calculé par l’algorithme
 
+
+    def set_debut(self, debut: int): 
+        self.debut = debut
+        self.fin = debut + self.duree 
+
---
+++ tests/test_tache.py
@@ -0,0 +1,15 @@
+from src.tache import Tache
+
+def test_tache_creation():
+    t = Tache("A", 3, [])
+    assert t.id == "A"
+    assert t.duree == 3
+    assert t.prealables == []
+    assert t.debut is None
+    assert t.fin is None
+
+def test_set_debut():
+    t = Tache("B", 4, ["A"])
+    t.set_debut(2)
+    assert t.debut == 2
+    assert t.fin == 6  # 2 + 4
